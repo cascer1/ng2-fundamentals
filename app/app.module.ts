32904b5e0e9f625f872fa67291a829d988e30818
Cas Eliëns <cas.eliens@gmail.com>
2017-10-12 14:32:58 +0200

    Implement basic routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,25 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {EventsAppComponent} from "./events-app.component";
+import {EventService} from "./events/shared/event.service";
 import {EventsListComponent} from "./events/events-list.component";
 import {EventThumbnailComponent} from "./events/event-thumbnail.component";
+import {EventDetailsComponent} from "./events/event-details/event-details.component";
 import {NavBarComponent} from "./nav/navbar.component";
-import {EventService} from "./events/shared/event.service";
 import {ToastrService} from "./common/toastr.service";
+import {appRoutes} from "./routes";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
     declarations: [
         EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
+        EventDetailsComponent,
         NavBarComponent
     ],
     providers: [
