d45c1b0f21c8e1a241b458c98dac058254bf83ae
Cas Eliëns <cas.eliens@gmail.com>
2017-10-12 15:17:13 +0200

    Add canDeactivate routeGuard to new event form

--- app/app.module.ts
+++ app/app.module.ts
@@ -30,10 +30,21 @@ import {EventRouteActivator} from "./events/event-details/event-route-activator.
     providers: [
         EventService,
         ToastrService,
-        EventRouteActivator
+        EventRouteActivator,
+        {
+            provide: 'canDeactivateCreateEvent',
+            useValue: checkDirtyState
+        }
     ],
     bootstrap: [EventsAppComponent]
 })
 export class AppModule {
 
+}
+
+function checkDirtyState(component: CreateEventComponent) {
+    if(component.isDirty)
+        return window.confirm('You have not saved this event, do you really want to cancel?')
+
+    return true;
 }
