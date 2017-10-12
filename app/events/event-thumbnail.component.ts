0a0812a8eb48b0d805941a16bb45d4a1db8e934d
Cas Eliëns <cas.eliens@gmail.com>
2017-10-12 13:40:52 +0200

    Complete module: Exploring the Angular Template Syntax

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import {Component, Input} from "@angular/core";
         <div class="well hoverwell thumbnail">
             <h2>{{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
-            <div>Time: {{event?.time}}</div>
+            <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
             <div>Price: \${{event?.price}}</div>
             <div *ngIf="event?.location">
                 <span>Location: {{event?.location?.address}}</span>
@@ -15,12 +20,27 @@ import {Component, Input} from "@angular/core";
             <div *ngIf="event?.onlineUrl">Online URL: {{event?.onlineUrl}}</div>
         </div>
     `,
-    styles: [`
-        .pad-left { margin-left: 10px; }
-        .well div { color: #bbb; }
-        .thumbnail { min-height: 210px; }
+    styles: [`        
+        .pad-left {
+            margin-left: 10px;
+        }
+
+        .well div {
+            color: #bbb;
+        }
+
+        .thumbnail {
+            min-height: 210px;
+        }
     `]
 })
 export class EventThumbnailComponent {
     @Input() event: any;
+
+    getStartTimeStyle():any {
+        if (this.event && this.event.time === '8:00 am')
+            return {color: '#030', 'font-weight': 'bold'};
+
+        return {};
+    }
 }
