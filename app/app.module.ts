5390c6fae9b67c503a3007963f01cba18457faa9
Cas Eliëns <cas.eliens@gmail.com>
2017-10-12 16:27:03 +0200

    Add index files to simplify imports

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,21 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {EventsAppComponent} from "./events-app.component";
-import {EventService} from "./events/shared/event.service";
-import {EventsListComponent} from "./events/events-list.component";
-import {EventThumbnailComponent} from "./events/event-thumbnail.component";
-import {EventDetailsComponent} from "./events/event-details/event-details.component";
 import {NavBarComponent} from "./nav/navbar.component";
 import {ToastrService} from "./common/toastr.service";
 import {appRoutes} from "./routes";
 import {RouterModule} from "@angular/router";
-import {CreateEventComponent} from "./events/create-event.component";
 import {Error404Component} from "./errors/404.component";
-import {EventRouteActivator} from "./events/event-details/event-route-activator.service";
-import {EventListResolver} from "./events/events-list-resolver.service";
+
+import {
+    CreateEventComponent,
+    EventDetailsComponent,
+    EventListResolver,
+    EventRouteActivator,
+    EventService,
+    EventsListComponent,
+    EventThumbnailComponent
+} from './events/index'
 
 @NgModule({
     imports: [
@@ -45,7 +48,7 @@ export class AppModule {
 }
 
 function checkDirtyState(component: CreateEventComponent) {
-    if(component.isDirty)
+    if (component.isDirty)
         return window.confirm('You have not saved this event, do you really want to cancel?');
 
     return true;
